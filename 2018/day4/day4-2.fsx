851202c1b7ba81bdbfc0c225abee766ff439959e
Andrew Smith <videogame0201@gmail.com>
2018-12-04 22:14:18 -0500

    Day 4 solutions

---
+++ 2018/day4/day4-2.fsx
@@ -0,0 +1,71 @@
+open System
+
+type Event = 
+    | BeginsShift of int
+    | FallsAsleep
+    | WakesUp
+
+type Record = { dateTime: DateTime; event: Event}
+
+type State = { 
+    currentGuard: int; 
+    fellAsleep: int option; 
+    guardLog: Map<int, int list>
+}
+
+let readInputFile () = 
+    System.IO.File.ReadAllLines("./2018/day4/input.txt") 
+    |> Array.toList
+
+let parseEvent (s: string) = 
+    match s.[0] with
+    | 'G' -> 
+        s.Split([|' '|])
+        |> Array.item 1
+        |> (fun s -> s.Replace("#", ""))
+        |> int
+        |> BeginsShift
+    | 'f' -> FallsAsleep
+    | 'w' -> WakesUp
+    | _ -> failwith "Invalid input"
+let logGuardMinutes (state: State) (record: Record) : State = 
+    match record.event with
+    | BeginsShift i -> 
+        let guardLog = 
+            match Map.tryFind i state.guardLog with
+            | Some _ -> state.guardLog
+            | None -> Map.add i [] state.guardLog
+        {state with currentGuard=i; fellAsleep=None; guardLog=guardLog}
+    | FallsAsleep -> {state with fellAsleep = Some record.dateTime.Minute}
+    | WakesUp -> 
+        let fellAsleepAt = 
+            match state.fellAsleep with
+            | Some i -> i
+            | None -> failwith "Can't wake when not asleep"
+        let range = [fellAsleepAt..record.dateTime.Minute-1]
+        let minuteList = Map.find state.currentGuard state.guardLog
+        let list = List.append range minuteList
+        let guardLog = Map.add state.currentGuard list state.guardLog
+        {state with fellAsleep=None; guardLog=guardLog}    
+
+let parseRecord (s: string) = 
+    let dateTime = DateTime.Parse(s.Substring(1, 16))
+    let event = parseEvent (s.Substring(19))
+    {dateTime = dateTime; event = event}
+
+let findGuard (state: State) = 
+    state.guardLog
+    |> Map.toList
+    |> List.filter (fun (_, m) -> List.length m > 0)
+    |> List.map (fun (i, m) -> (i, (List.countBy id m |> List.maxBy snd)))
+    |> List.maxBy (fun (_,(_, c)) -> c)
+    |> fun (g, (m, _)) -> g * m
+
+
+let main() = 
+    readInputFile()
+    |> List.map parseRecord
+    |> List.sortBy (fun x -> x.dateTime)
+    |> List.fold logGuardMinutes {currentGuard=0; fellAsleep=None; guardLog=Map.empty}
+    |> findGuard
+    |> printfn "Answer is: %i"
