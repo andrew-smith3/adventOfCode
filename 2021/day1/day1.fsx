1f606c46b8bb410e64ebd9408d66def252c0d04c
Andrew Smith <videogame0201@gmail.com>
2021-12-01 19:07:28 -0500

    2021 day1

---
+++ 2021/day1/day1.fsx
@@ -0,0 +1,11 @@
+﻿let readFile () = 
+    System.IO.File.ReadLines("input.txt")
+    |> Seq.map int
+    
+let main () =
+    readFile ()
+    |> Seq.pairwise
+    |> Seq.fold (fun s (x, y) -> if x < y then s + 1 else s) 0
+    |> fun x -> printfn "Answer: %d" x
+    
+main ()
---
+++ 2021/day1/day1pt2.fsx
@@ -0,0 +1,13 @@
+﻿let readFile () = 
+    System.IO.File.ReadLines("input.txt")
+    |> Seq.map int
+    
+let main () =
+    readFile ()
+    |> Seq.windowed 3
+    |> Seq.map Array.sum
+    |> Seq.pairwise
+    |> Seq.fold (fun s (x, y) -> if x < y then s + 1 else s) 0
+    |> fun x -> printfn "Answer: %d" x
+    
+main ()
