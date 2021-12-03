67814f37fc1eca98feb6f9e8e503d165427a1e62
Andrew Smith <videogame0201@gmail.com>
2021-12-02 19:29:09 -0500

    2021 day2

---
+++ 2021/day2/day2.fsx
@@ -0,0 +1,25 @@
+﻿let parseLines (s: string) =
+    s.Split ' '
+    |> (fun x -> x.[0], (int x.[1]))
+
+let readFile () = 
+    System.IO.File.ReadLines("input.txt")
+    |> Seq.map parseLines
+    
+    
+let processCommands ((h, d): int * int) ((command, amount): string * int) =
+    match command with
+    | "forward" -> (h + amount, d)
+    | "down" -> (h, d + amount)
+    | "up" -> (h, d - amount)
+    | _ -> (h, d)
+    
+let main () =
+    readFile ()
+    |> Seq.fold processCommands (0, 0)
+    |> fun (h, d) -> h * d
+    |> fun x -> printfn $"Answer: %d{x}"
+    
+main ()
+    
+   
---
+++ 2021/day2/day2pt2.fsx
@@ -0,0 +1,25 @@
+﻿let parseLines (s: string) =
+    s.Split ' '
+    |> (fun x -> x.[0], (int x.[1]))
+
+let readFile () = 
+    System.IO.File.ReadLines("input.txt")
+    |> Seq.map parseLines
+    
+    
+let processCommands ((h, d, a): int * int * int) ((command, amount): string * int) =
+    match command with
+    | "forward" -> (h + amount, d + (a * amount), a)
+    | "down" -> (h, d, a + amount)
+    | "up" -> (h, d, a - amount)
+    | _ -> (h, d, a)
+    
+let main () =
+    readFile ()
+    |> Seq.fold processCommands (0, 0, 0)
+    |> fun (h, d, _) -> h * d
+    |> fun x -> printfn $"Answer: %d{x}"
+    
+main ()
+    
+   
