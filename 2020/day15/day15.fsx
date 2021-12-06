dadd139b7a292e4adcf0cf9285a7849687f35f6e
Andrew Smith <videogame0201@gmail.com>
2021-12-05 22:53:16 -0500

    2020 day 15

---
+++ 2020/day15/day15.fsx
@@ -0,0 +1,23 @@
+﻿
+let test = [(0,1); (3,2); (6,3)] |> List.rev
+
+let input = [(16,1); (1,2); (0,3); (18,4); (12, 5); (14, 6); (19, 7)] |> List.rev
+
+let unfold ((list, currentTurn): (int * int) list * int) =
+    let (h, turnBefore)::tail = list
+    match List.tryFind (fun (x, _) -> x = h) tail with
+    | Some (_, lastTurnCalled) ->
+        let numberToCall = turnBefore - lastTurnCalled
+        Some((numberToCall, currentTurn), ((numberToCall, currentTurn)::list, currentTurn+1))
+    | None -> Some ((0, currentTurn), ((0, currentTurn)::list, currentTurn+1))
+    
+
+let main startList turnToFind =
+    let nextTurn = startList |> List.length |> (+) 1 
+    Seq.unfold unfold (startList, nextTurn)
+    |> Seq.find (fun (_,t) -> t = turnToFind)
+    |> fst
+    |> fun x -> printfn $"Answer: %d{x}"
+    
+main input 2020
+
---
+++ 2020/day15/day15pt2.fsx
@@ -0,0 +1,24 @@
+﻿
+let test = Map [(0,1); (3,2); (6,3)]
+
+let input = Map [(16,1); (1,2); (0,3); (18,4); (12, 5); (14, 6); (19, 7)]
+
+let unfold ((map, (lastCalled, lastTurn)): Map<int,int> * (int * int) ) =
+    let currentTurn = lastTurn + 1
+    let numberToCall =
+        match Map.tryFind lastCalled map with
+        | Some previousCalledTurn ->
+            lastTurn - previousCalledTurn
+        | None -> 0
+    let map = Map.add lastCalled lastTurn map
+    let next = numberToCall, currentTurn
+    Some (next, (map, next))
+    
+let main (map: Map<int, int>) (turnToFind: int) =
+    let last = Map.toList map |> List.maxBy snd
+    Seq.unfold unfold (map, last)
+    |> Seq.find (fun (_,t) -> t = turnToFind)
+    |> fst
+    |> fun x -> printfn $"Answer: %d{x}"
+    
+main input 30000000
